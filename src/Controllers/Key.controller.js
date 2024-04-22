354f9b53742f66323c37b1afa187528d205b3dc0
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-04-22 23:42:24 +0530

    Added key controller

---
+++ src/Controllers/Key.controller.js
@@ -0,0 +1,32 @@
+const AsyncHandler = require("../Utils/AsyncHandler");
+const ErrorHandler = require("../Utils/ErrorHandler");
+const key = require("../Model/APIKey.model");
+
+const getAPIKeys = AsyncHandler(async(req,res,next)=>{
+    const {_id} = req.body
+    // const {_id} = req.user
+    const user = _id
+    const responce = await key.find({user}).populate('user')
+    res.status(200).json({success:true,message:"Keys found ",responce})
+})
+
+const createKey = AsyncHandler(async(req,res,next)=>{
+    const {_id} = req.body
+    //const {_id} = req.user
+    const user = _id
+    const random = Math.ceil(Math.random()*1000000)
+    console.log(random)
+    const responce = await key.create({user,APIkey:random+1000000})
+    console.log(responce)
+    res.status(200).json({success:true,message:"Keys created ",responce})
+})
+
+const deleteKey = AsyncHandler(async(req,res,next)=>{
+    const {_id} = req.body
+    const responce = await key.findByIdAndDelete({_id})
+    if(!responce)
+        return next(new ErrorHandler("API key doesn't exist",404))
+    res.status(200).json({success:true,message:"API Key deleted successfully"})
+})
+
+module.exports = {getAPIKeys,createKey,deleteKey}
