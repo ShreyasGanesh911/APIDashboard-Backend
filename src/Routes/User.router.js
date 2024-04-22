c49c4ec0b66f21f9ebca755291fc578bf6355ec3
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-04-22 23:42:38 +0530

    Added key routes

--- src/Routes/User.router.js
+++ src/Routes/User.router.js
@@ -1,8 +1,12 @@
 const express = require("express")
 const { createUser, loginUser } = require("../Controllers/User.controller")
+const { getAPIKeys, createKey, deleteKey } = require("../Controllers/Key.controller")
 const userRouter = express.Router()
 
 userRouter.post('/signup',createUser)
 userRouter.post('/login',loginUser)
 
+userRouter.post('/getKeys',getAPIKeys)
+userRouter.post('/createKey',createKey)
+userRouter.delete("/deleteKey",deleteKey)
 module.exports = userRouter
