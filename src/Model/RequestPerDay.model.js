7c4792d825a505111570c24500dbe038567afb3c
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-04-24 11:32:24 +0530

    Made new model

---
+++ src/Model/RequestPerDay.model.js
@@ -0,0 +1,21 @@
+const mongoose  = require("mongoose");
+
+const requestSchema = new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"user",
+        unique:false
+    },
+    date:{
+        type:String,
+        required:[true,"date not provided"]
+    },
+    requests:{
+        type:Number,
+        default:0
+    }
+
+})
+
+const request = mongoose.model('request',requestSchema)
+module.exports = request
