b926398786fb9a2c32a7fa4a74d069838617e1d4
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-04-24 11:32:02 +0530

    Added routes

--- src/Routes/User.router.js
+++ src/Routes/User.router.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const { createUser, loginUser } = require("../Controllers/User.controller")
-const { getAPIKeys, createKey, deleteKey } = require("../Controllers/Key.controller")
+const { getAPIKeys, createKey, deleteKey, APIRequests } = require("../Controllers/Key.controller")
 const auth = require("../Middleware/Auth")
+const { makeRequest } = require("../Controllers/API.controller")
 const userRouter = express.Router()
 
 userRouter.post('/signup',createUser)
@@ -10,4 +11,7 @@ userRouter.post('/login',loginUser)
 userRouter.post('/getKeys',auth,getAPIKeys)
 userRouter.post('/createKey',auth,createKey)
 userRouter.delete("/deleteKey",auth,deleteKey)
+userRouter.get('/allRequests',auth,APIRequests)
+
+userRouter.get('/make',makeRequest)
 module.exports = userRouter
