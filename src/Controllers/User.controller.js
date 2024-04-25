3df5a24df941cd0a3779641fc67d839f0cd80c94
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-04-25 11:51:41 +0530

    Added about user function

--- src/Controllers/User.controller.js
+++ src/Controllers/User.controller.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 const user = require("../Model/User.model");
 const AsyncHandler = require("../Utils/AsyncHandler");
 const ErrorHandler = require("../Utils/ErrorHandler");
-
+const month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dec"];
 const createUser = AsyncHandler(async(req,res,next)=>{
     const {phone,name,email,password} = req.body
 
@@ -29,14 +29,11 @@ const aboutUser = AsyncHandler(async(req,res,next)=>{
     const date = new Date()
     const {_id} = req.users
     const responce = await user.findById({_id}).select('-password')
-
-    console.log(responce)
-    console.log(Math.floor((responce.endDate.getTime() - date.getTime() )/(1000 * 60 * 60 * 24)))
     const result = {
         name:responce.name,
-        start:responce.date,
-        end:responce.endDate,
-        remaining:Math.floor((responce.endDate.getTime() - date.getTime() )/(1000 * 60 * 60 * 24))
+        start:`${responce.date.getDate()}-${month[responce.date.getMonth()]}`,
+        end:`${responce.endDate.getDate()}-${month[responce.endDate.getMonth()]}`,
+        remaining:Math.floor((responce.endDate.getTime()-date.getTime() )/(1000 * 60 * 60 * 24))
     }
     res.status(200).json({success:true,responce:result})
 })
