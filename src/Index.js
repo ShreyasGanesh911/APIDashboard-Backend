e6c8046b9b8fea2f0195ed7e7c6a6fcb5fca2cce
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-04-26 09:50:43 +0530

    Added port number

--- src/Index.js
+++ src/Index.js
@@ -1,9 +1,11 @@
 const app = require("./App");
+require("dotenv").config()
+const PORT = process.env.PORT || 4000
 const connection = require("./database");
 
 connection().then(()=>{
-    app.listen(4000,()=>{
-        console.log("Active at port 4000")
+    app.listen(PORT,()=>{
+        console.log("Active at port ",PORT)
     })
 }).catch((e)=>{
     console.log("Error connecting to mongodb")
