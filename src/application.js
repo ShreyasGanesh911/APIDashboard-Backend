91b45f995b6db54fe7fcc12ed1e4030909154deb
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-04-26 16:03:27 +0530

    CORS changes

--- src/application.js
+++ src/application.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({origin:"http://localhost:3000",credentials: true}))
+app.use(cors({credentials: true}))
 app.use('/user',userRouter)
 app.use(APIError)
 
