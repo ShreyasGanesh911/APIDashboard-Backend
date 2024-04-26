a0e03a9a74dc0d817f58eb5000d4187c6134d4fb
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-04-26 16:31:59 +0530

    cors origin changed

--- src/application.js
+++ src/application.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({origin:"https://api-dashboard-frontend-six.vercel.app/",credentials: true}))
+app.use(cors({origin:"*",credentials: true}))
 app.use('/user',userRouter)
 app.use(APIError)
 
